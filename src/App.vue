6c6a701f24f7578aa774ef6222c1635d899b2a3e
moonlightboss <cumrak.legenda@gmail.com>
2023-03-22 17:43:19 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col sm="6" offset-sm="3">
+          <PomodoroClock/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import PomodoroClock from './components/PomodoroClock.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    PomodoroClock,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/components/PomodoroClock.vue
@@ -0,0 +1,93 @@
+<template>
+    <v-card class="mt-8">
+        <v-tabs v-model="timerType" grow>
+            <v-tab
+                v-for="tab in tabsTitles"
+                :key="tab"
+            >
+                {{ tab }}
+            </v-tab>
+        </v-tabs>
+            <v-window v-model="timerType">
+                <v-window-item>
+                     <v-card 
+                        class="pa-5 d-flex flex-column justify-center align-center"
+                        flat
+                     >
+                     <h1 class="time">{{ displayMinutes }}:{{ displaySeconds }}</h1>
+                     <div class="button-group">
+                        <v-btn @click="start" color="primary">
+                            <v-icon left small>mdi-play-circle-outline</v-icon>
+                            Start
+                        </v-btn>
+                        <v-btn @click="stop" color="error">
+                            <v-icon left small>mdi-stop-circle-outline</v-icon>
+                            Stop
+                        </v-btn>
+                        <v-btn @click="reset">
+                            <v-icon left small>mdi-restart</v-icon>
+                            Reset
+                        </v-btn>
+                    </div>
+                     </v-card>    
+                </v-window-item>
+            </v-window>
+    </v-card>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            display:{
+                minutes:'00',
+                seconds:'00',  
+            },
+            totalSeconds: 25 * 60,
+            timerType:0,
+            tabsTitles: ['Pomodoro','Short break','Long Break']
+        }
+    },
+    computed:{
+        displayMinutes(){
+            const minutes = Math.floor(this.totalSeconds/60)
+            return this.formatTime(minutes)
+        },
+        displaySeconds(){
+            let seconds = this.totalSeconds % 60
+            return this.formatTime(seconds)
+        },
+    },
+    methods:{
+        formatTime(time){ //Создаётся что бы во время работы таймера цифры не сьезжали.
+            if (time < 10){
+                return '0' + time
+            }
+            return time.toString()
+        },
+        start(){
+            setInterval(()=>{
+                this.totalSeconds -=1
+            },1000)
+        },
+        stop(){
+
+        },
+        reset(){
+
+        }
+    }
+}
+</script>
+<style>
+.v-card{
+    width: 600px;
+}
+.time{
+    font-size: 80px;
+    font-weight: 400;
+    text-align: center;
+}
+.v-btn{
+    margin: 0 2px;
+}
+</style>
